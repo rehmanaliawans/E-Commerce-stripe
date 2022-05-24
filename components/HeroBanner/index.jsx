75b1c0f6e0396dd45bdcec189bcbe194cbb719b8
Rehman ali <rehmanaliawans@gmail.com>
2022-05-25 01:27:30 +0500

    get data from sanity and show on home page

--- components/HeroBanner/index.jsx
+++ components/HeroBanner/index.jsx
@@ -1,22 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import { Box, Button, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-const HeroBanner = () => {
+import { urlFor } from "../../lib/client";
+const HeroBanner = ({ HeroBanner }) => {
   return (
     <Grid className="hero-banner-container">
       <Grid>
         <Typography variant="body1" className="beats-solo">
-          SMALL TEXT
+          {HeroBanner.smallText}
         </Typography>
-        <Typography variant="h3">MID TEXT</Typography>
-        {/* <Image scr="" alt="headephones" className="hero-banner-image" /> */}
+        <Typography variant="h3">{HeroBanner.midText}</Typography>
+        <Typography variant="h1">{HeroBanner.largeText1}</Typography>
+        <img
+          src={urlFor(HeroBanner.image)}
+          alt="headephones"
+          className="hero-banner-image"
+        />
         <Grid>
-          <Link href="/product /ID">
-            <Button> BUTTON TEXT</Button>
+          <Link href={`/product/${HeroBanner.product}`}>
+            <Button>{HeroBanner.buttonText}</Button>
           </Link>
           <Grid className="desc">
             <Typography variant="h5">Description</Typography>
-            <Typography variant="body1">DESCRIPTION</Typography>
+            <Typography variant="body1">{HeroBanner.desc}</Typography>
           </Grid>
         </Grid>
       </Grid>
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
 import { Grid, Typography, Box } from "@mui/material";
+import { client } from "../lib/client";
 import { Product, HeroBanner, FooterBanner } from "../components";
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner HeroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <Box className="products-heading">
         <Typography variant="h2">Best Selling Products</Typography>
         <Typography variant="body1">Speakers of many variantions</Typography>
       </Box>
       <Box className="products-container">
-        {["Product 1 ", "Product 2"].map((product, index) => product)}
+        {products?.map((product) => product.name)}
       </Box>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = "*[_type == 'product']";
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  };
 };
 
 export default Home;
